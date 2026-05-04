6e3cfbd2513d650286aecb0d8f391ab976f3ed83
vkoppak1 <vkoppak1@asu.edu>
2026-05-03 19:31:29 -0700

    Initial commit

---
+++ test/rds-template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RdsTemplate from '../lib/rds-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rds-template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RdsTemplate.RdsTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
